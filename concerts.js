8988095444fcb3fe2a60e9b83cfc5da15f4eeecc
Federico <federico@Federicos-MacBook-Pro.local>
2019-01-23 13:31:24 -0800

    concert

--- concerts.js
+++ concerts.js
@@ -1,5 +1,28 @@
+var axios = require("axios");
+var moment = require('moment');
+
 function getConcert(concert){
-    console.log('This is getConcert. You asked for: ' + concert);
+    
+    var url = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp";
+
+    axios.get(url).then(
+        function(response) {
+            
+            response.data.forEach(element => {
+                console.log('-------------------');
+                console.log('Artist: '+ element.lineup[0]);
+                console.log('Lineup: '+ element.lineup);
+                console.log('Venue Name: ' + element.venue.name);
+                console.log('Venue City: ' + element.venue.city);
+                console.log('Venue State: ' + element.venue.region);
+                console.log('Date: '+ moment(element.datetime).format('LLLL'));
+                console.log('-------------------');
+            });
+
+            return response;
+        }
+    ); 
+    
 }
 
 module.exports = {
